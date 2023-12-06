b8913a4a8187c24d04ebfcdbbbbf9abd78ed7210
Simon Lilja <simlil@kth.se>
2023-12-06 23:59:02 +0100

    Sidebar done?

--- src/CommuteModel.js
+++ src/CommuteModel.js
@@ -1,8 +1,7 @@
 //array with objects
 //struct /object
 
-import { getTimeDetails } from "./timetableSource";
-import { getStationID } from "./timetableSource";
+import { getTimeDetails, getStationID } from "./timetableSource";
 import resolvePromise from "./resolvePromise.js";
 
 //attributes:
@@ -24,6 +23,29 @@ import resolvePromise from "./resolvePromise.js";
 
 export default {
 
+    /*locations: [
+        {
+            id: 1,
+            name: 'Default Location',
+            stations: [
+                {
+                    id: 345,
+                    distance: 6,
+                    lines: [345, 23, 95]
+                }
+            ],
+        }
+    ],*/
+    currentLocation: 1,
+    locations: [
+        {
+            id: 1,
+            name: 'Default Location',
+            stations: [],
+        },
+    ],
+    addingLocation: false,
+
     commuteStations: [],
     commuteDistances: [],
     intrestedLines: [],
@@ -32,6 +54,43 @@ export default {
 
     stationIDPromiseState: {},
     stationDetailsPromiseState: {},
+
+    lastLocation() {
+        if (this.locations.length < 2) {
+            return true;
+        }
+        return false;
+    },
+
+    removeLocation(locationID) {
+        this.locations = this.locations.filter(location => location.id !== locationID);
+        this.currentLocation = this.locations[0].id;
+    },
+
+    addLocation(locationName) {
+        const locationID = this.locations.slice(-1)[0].id + 1;
+        this.locations = [
+            ...this.locations,
+            {
+                id: locationID,
+                name: locationName,
+                stations: [],
+            }
+        ];
+        this.currentLocation = locationID;
+    },
+
+    addLocationToggle() {
+        this.addingLocation = !this.addingLocation;
+    },
+
+    setLocation(locationID) {
+        this.currentLocation = locationID;
+    },
+
+    getLocation() {
+        return this.locations.find(location => location.id === this.currentLocation);
+    },
 
     setCurrentStation(stationName){
         stationName = 'Slussen';
