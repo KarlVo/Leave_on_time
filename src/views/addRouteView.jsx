55c29c39228c0e6f29a978154d8cc33d6786a3c8
C4g0 <karlvo@kth.se>
2023-12-09 11:36:57 +0100

    slider added

--- src/views/addRouteView.jsx
+++ src/views/addRouteView.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-
+import Slider from '@mui/material/Slider';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import FormControl from '@mui/material/FormControl';
@@ -10,13 +10,21 @@ import Autocomplete from '@mui/material/Autocomplete';
 import metroArray from '/src/metrosArray';
 import InputLabel from '@mui/material/InputLabel';
 import SaveIcon from '@mui/icons-material/Save';
+import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
+import { styled } from '@mui/material/styles';
+
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+import MuiInput from '@mui/material/Input';
+import VolumeUp from '@mui/icons-material/VolumeUp';
 
 function addRouteView(props) {
     //const spacing = '20px'; // Adjust the spacing as needed
     
     const [selectedValue, setSelectedValue] = useState('');
     const numberOptions = Array.from({ length: 60 }, (_, index) => index + 1); 
-   
+    const [value, setValue] = React.useState(30);
     
     function textChangeStation(event){
         console.log("textChangeStation");
@@ -33,12 +41,13 @@ function addRouteView(props) {
         props.textLine(event.target.value);
       }
     
+    /*
     function handleChange(event) {
         console.log("handleChange");
         setSelectedValue(event.target.value);
         props.textMin(event.target.value);
       }
-
+      */
     function closeAddRoute(){
         console.log("closeAddRoute");
         window.location.hash="#/";
@@ -48,13 +57,44 @@ function addRouteView(props) {
       console.log("saveRoute");
       props.submitEvent();
     }
+    const Input = styled(MuiInput)`
+  width: 42px;
+`;
+
+
+  const handleSliderChange = (event, newValue) => { // slider code from https://mui.com/material-ui/react-slider/
+    console.log("handleSliderChange");
+    setValue(newValue);
+    props.textMin(event.target.value);
     
+  };
+
+  const handleInputChange = (event) => {
+    console.log("handleInputChange");
+    setValue(event.target.value === '' ? 0 : Number(event.target.value));
+    props.textMin(event.target.value);
+  };
+
+  const handleBlur = () => {
+    if (value < 0) {
+      setValue(0);
+    } else if (value > 100) {
+      setValue(100);
+    }
+  };
+      
     return (
       <Box display="flex" flexDirection='column'>
        
-        <Box display="flex" justifyContent="flex-end">
+       
+        <Box display="flex" justifyContent="space-between" alignItems="center" marginTop={2}>
+        <Box>
+          <SaveIcon onClick={saveRoute} />
+        </Box>
+        <Box>
           <CloseIcon onClick={closeAddRoute} />
         </Box>
+      </Box>
         <Box flexDirection='column' marginTop={2} marginBottom={2}>
         
           <Autocomplete
@@ -66,25 +106,30 @@ function addRouteView(props) {
         renderInput={(params) => <TextField {...params} label="New Station" onChange={textChangeStation} variant="outlined" id="outlined-basic" fullWidth />}
 />
         </Box>
-          <FormControl variant="outlined" fullWidth>
-          <InputLabel htmlFor="time-to-station">Time to Station</InputLabel>
-              <Select label="Time to Station" value={selectedValue} onChange={handleChange}>
-                  <MenuItem value="">Select number of minutes</MenuItem>
-                  {numberOptions.map((number) => (
-                  <MenuItem key={number} value={number}>{number}</MenuItem>  
-                  ))}
-              </Select>
-          </FormControl>
-        
         <Box flexDirection='column' marginTop={2} marginBottom={2}>
           <TextField onChange={textChangeTowards} id="outlined-basic" label="Towards" variant="outlined" fullWidth />
         </Box>
-        <Box flexDirection='column' marginBottom={2}>
-          <TextField onChange={textChangeLine} id="outlined-basic" label="Line" variant="outlined" fullWidth/>
-        </Box>
-        <Box display="flex">
-          <SaveIcon onClick={saveRoute} />
-        </Box>
+
+       
+<Box flexDirection='column' marginBottom={2}>
+      <Box sx={{ width: 250 }}>
+        <Typography id="input-slider" gutterBottom>Time to Station</Typography>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item>
+            <DirectionsRunIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider value={typeof value === 'number' ? value : 0} onChange={handleSliderChange} aria-labelledby="input-slider" min={0} max={20}
+            />
+          </Grid>
+          <Grid item>
+            <Input value={value} size="small" onChange={handleInputChange} onBlur={handleBlur} inputProps={{ step: 1, min: 0, max: 30, type: 'number', 'aria-labelledby': 'input-slider', }} />
+            <span style={{ marginLeft: '4px' }}>min</span>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+      
       </Box>
     );
   }
