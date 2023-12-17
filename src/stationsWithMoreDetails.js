4fcf1f451e1d117a65f2e2445ce346477f18b3d0
redAfroo <64847741+redAfroo@users.noreply.github.com>
2023-12-17 15:51:04 +0100

    ArrayWithInfo
    ArrayWithInfo

---
+++ src/stationsWithMoreDetails.js
@@ -0,0 +1,608 @@
+const infoArray = [
+    {
+      "StationName": "Abrahamsberg",
+      "SiteId": "9110",
+      "X": "17952753",
+      "Y": "59336869"
+    },
+    {
+      "StationName": "Akalla",
+      "SiteId": "9300",
+      "X": "17913281",
+      "Y": "59415461"
+    },
+    {
+      "StationName": "Alby",
+      "SiteId": "9282",
+      "X": "17846608",
+      "Y": "59239192"
+    },
+    {
+      "StationName": "Alvik",
+      "SiteId": "9112",
+      "X": "17980628",
+      "Y": "59332940"
+    },
+    {
+      "StationName": "Aspudden",
+      "SiteId": "9293",
+      "X": "18002184",
+      "Y": "59305694"
+    },
+    {
+      "StationName": "Axelsberg",
+      "SiteId": "9291",
+      "X": "17977410",
+      "Y": "59304031"
+    },
+    {
+      "StationName": "Bagarmossen",
+      "SiteId": "9141",
+      "X": "18133508",
+      "Y": "59276596"
+    },
+    {
+      "StationName": "Bandhagen",
+      "SiteId": "9163",
+      "X": "18048272",
+      "Y": "59270555"
+    },
+    {
+      "StationName": "Bergshamra",
+      "SiteId": "9202",
+      "X": "18038321",
+      "Y": "59380116"
+    },
+    {
+      "StationName": "Björkhagen",
+      "SiteId": "9143",
+      "X": "18116078",
+      "Y": "59291356"
+    },
+    {
+      "StationName": "Blackeberg",
+      "SiteId": "9105",
+      "X": "17884767",
+      "Y": "59349562"
+    },
+    {
+      "StationName": "Blåsut",
+      "SiteId": "9187",
+      "X": "18091052",
+      "Y": "59289532"
+    },
+    {
+      "StationName": "Bredäng",
+      "SiteId": "9289",
+      "X": "17933390",
+      "Y": "59294736"
+    },
+    {
+      "StationName": "Brommaplan",
+      "SiteId": "9109",
+      "X": "17937867",
+      "Y": "59338298"
+    },
+    {
+      "StationName": "Danderyds sjukhus",
+      "SiteId": "9201",
+      "X": "18042609",
+      "Y": "59390058"
+    },
+    {
+      "StationName": "Duvbo",
+      "SiteId": "9324",
+      "X": "17965194",
+      "Y": "59367801"
+    },
+    {
+      "StationName": "Enskede gård",
+      "SiteId": "9167",
+      "X": "18070359",
+      "Y": "59289639"
+    },
+    {
+      "StationName": "Farsta",
+      "SiteId": "9180",
+      "X": "18101237",
+      "Y": "59236064"
+    },
+    {
+      "StationName": "Farsta strand",
+      "SiteId": "9180",
+      "X": "18101237",
+      "Y": "59236064"
+    },
+    {
+      "StationName": "Fittja",
+      "SiteId": "9283",
+      "X": "17861243",
+      "Y": "59247956"
+    },
+    {
+      "StationName": "Fridhemsplan",
+      "SiteId": "9115",
+      "X": "18029134",
+      "Y": "59332131"
+    },
+    {
+      "StationName": "Fruängen",
+      "SiteId": "9260",
+      "X": "17965454",
+      "Y": "59285594"
+    },
+    {
+      "StationName": "Gamla stan",
+      "SiteId": "9193",
+      "X": "18068192",
+      "Y": "59323016"
+    },
+    {
+      "StationName": "Globen",
+      "SiteId": "9168",
+      "X": "18076040",
+      "Y": "59293667"
+    },
+    {
+      "StationName": "Gubbängen",
+      "SiteId": "9183",
+      "X": "18082782",
+      "Y": "59261962"
+    },
+    {
+      "StationName": "Gullmarsplan",
+      "SiteId": "9189",
+      "X": "18080939",
+      "Y": "59298422"
+    },
+    {
+      "StationName": "Gärdet",
+      "SiteId": "9221",
+      "X": "18102279",
+      "Y": "59348087"
+    },
+    {
+      "StationName": "Hagsätra",
+      "SiteId": "9160",
+      "X": "18016657",
+      "Y": "59261611"
+    },
+    {
+      "StationName": "Hallonbergen",
+      "SiteId": "9303",
+      "X": "17970192",
+      "Y": "59375567"
+    },
+    {
+      "StationName": "Hallunda",
+      "SiteId": "9281",
+      "X": "17825942",
+      "Y": "59243282"
+    },
+    {
+      "StationName": "Hammarbyhöjden",
+      "SiteId": "9144",
+      "X": "18107026",
+      "Y": "59294709"
+    },
+    {
+      "StationName": "Hjulsta",
+      "SiteId": "9320",
+      "X": "17885630",
+      "Y": "59395829"
+    },
+    {
+      "StationName": "Hornstull",
+      "SiteId": "9295",
+      "X": "18034231",
+      "Y": "59315537"
+    },
+    {
+      "StationName": "Husby",
+      "SiteId": "9301",
+      "X": "17927053",
+      "Y": "59410688"
+    },
+    {
+      "StationName": "Huvudsta",
+      "SiteId": "9327",
+      "X": "17985419",
+      "Y": "59349651"
+    },
+    {
+      "StationName": "Hägerstensåsen",
+      "SiteId": "9262",
+      "X": "17977185",
+      "Y": "59294467"
+    },
+    {
+      "StationName": "Hässelby gård",
+      "SiteId": "9101",
+      "X": "17843408",
+      "Y": "59366488"
+    },
+    {
+      "StationName": "Hässelby strand",
+      "SiteId": "9100",
+      "X": "17831398",
+      "Y": "59360870"
+    },
+    {
+      "StationName": "Högdalen",
+      "SiteId": "9162",
+      "X": "18041171",
+      "Y": "59263121"
+    },
+    {
+      "StationName": "Hökarängen",
+      "SiteId": "9182",
+      "X": "18082773",
+      "Y": "59258429"
+    },
+    {
+      "StationName": "Hötorget",
+      "SiteId": "9119",
+      "X": "18063868",
+      "Y": "59335529"
+    },
+    {
+      "StationName": "Islandstorget",
+      "SiteId": "9106",
+      "X": "17893352",
+      "Y": "59346415"
+    },
+    {
+      "StationName": "Johannelund",
+      "SiteId": "9102",
+      "X": "17855373",
+      "Y": "59367675"
+    },
+    {
+      "StationName": "Karlaplan",
+      "SiteId": "9222",
+      "X": "18092094",
+      "Y": "59339071"
+    },
+    {
+      "StationName": "Kista",
+      "SiteId": "9302",
+      "X": "17942595",
+      "Y": "59403470"
+    },
+    {
+      "StationName": "Kristineberg",
+      "SiteId": "9113",
+      "X": "18005897",
+      "Y": "59332707"
+    },
+    {
+      "StationName": "Kungsträdgården",
+      "SiteId": "9340",
+      "X": "18073343",
+      "Y": "59330801"
+    },
+    {
+      "StationName": "Kymlinge",
+      "SiteId": "225",
+      "X": "18489463",
+      "Y": "59111275"
+    },
+    {
+      "StationName": "Kärrtorp",
+      "SiteId": "9142",
+      "X": "18115736",
+      "Y": "59283725"
+    },
+    {
+      "StationName": "Liljeholmen",
+      "SiteId": "9294",
+      "X": "18024810",
+      "Y": "59310665"
+    },
+    {
+      "StationName": "Mariatorget",
+      "SiteId": "9297",
+      "X": "18062115",
+      "Y": "59317012"
+    },
+    {
+      "StationName": "Masmo",
+      "SiteId": "9284",
+      "X": "17878969",
+      "Y": "59249727"
+    },
+    {
+      "StationName": "Medborgarplatsen",
+      "SiteId": "9191",
+      "X": "18073433",
+      "Y": "59314288"
+    },
+    {
+      "StationName": "Midsommarkransen",
+      "SiteId": "9264",
+      "X": "18011983",
+      "Y": "59301874"
+    },
+    {
+      "StationName": "Mälarhöjden",
+      "SiteId": "9290",
+      "X": "17956546",
+      "Y": "59301280"
+    },
+    {
+      "StationName": "Mörby centrum",
+      "SiteId": "9200",
+      "X": "18036271",
+      "Y": "59398373"
+    },
+    {
+      "StationName": "Norsborg",
+      "SiteId": "9280",
+      "X": "17814085",
+      "Y": "59244091"
+    },
+    {
+      "StationName": "Näckrosen",
+      "SiteId": "9304",
+      "X": "17983756",
+      "Y": "59366443"
+    },
+    {
+      "StationName": "Odenplan",
+      "SiteId": "9117",
+      "X": "18049099",
+      "Y": "59342901"
+    },
+    {
+      "StationName": "Rinkeby",
+      "SiteId": "9322",
+      "X": "17922037",
+      "Y": "59389033"
+    },
+    {
+      "StationName": "Rissne",
+      "SiteId": "9323",
+      "X": "17941273",
+      "Y": "59376035"
+    },
+    {
+      "StationName": "Ropsten",
+      "SiteId": "9220",
+      "X": "18103430",
+      "Y": "59358002"
+    },
+    {
+      "StationName": "Råcksta",
+      "SiteId": "9104",
+      "X": "17881136",
+      "Y": "59355072"
+    },
+    {
+      "StationName": "Rådhuset",
+      "SiteId": "9309",
+      "X": "18044245",
+      "Y": "59331035"
+    },
+    {
+      "StationName": "Rådmansgatan",
+      "SiteId": "9118",
+      "X": "18057531",
+      "Y": "59341930"
+    },
+    {
+      "StationName": "Rågsved",
+      "SiteId": "9161",
+      "X": "18026617",
+      "Y": "59256496"
+    },
+    {
+      "StationName": "Sandsborg",
+      "SiteId": "9186",
+      "X": "18089631",
+      "Y": "59284830"
+    },
+    {
+      "StationName": "Sankt Eriksplan",
+      "SiteId": "9116",
+      "X": "18038636",
+      "Y": "59339583"
+    },
+    {
+      "StationName": "Skanstull",
+      "SiteId": "9190",
+      "X": "18076166",
+      "Y": "59307941"
+    },
+    {
+      "StationName": "Skarpnäck",
+      "SiteId": "9140",
+      "X": "18132618",
+      "Y": "59266456"
+    },
+    {
+      "StationName": "Skogskyrkogården",
+      "SiteId": "9185",
+      "X": "18092697",
+      "Y": "59279823"
+    },
+    {
+      "StationName": "Skärholmen",
+      "SiteId": "9287",
+      "X": "17909488",
+      "Y": "59276758"
+    },
+    {
+      "StationName": "Skärmarbrink",
+      "SiteId": "9188",
+      "X": "18091339",
+      "Y": "59297496"
+    },
+    {
+      "StationName": "Slussen",
+      "SiteId": "9192",
+      "X": "18072453",
+      "Y": "59320320"
+    },
+    {
+      "StationName": "Sockenplan",
+      "SiteId": "9166",
+      "X": "18071536",
+      "Y": "59282511"
+    },
+    {
+      "StationName": "Solna centrum",
+      "SiteId": "9305",
+      "X": "17997555",
+      "Y": "59358677"
+    },
+    {
+      "StationName": "Solna strand",
+      "SiteId": "9326",
+      "X": "17974381",
+      "Y": "59353499"
+    },
+    {
+      "StationName": "Stadion",
+      "SiteId": "9205",
+      "X": "18081532",
+      "Y": "59340518"
+    },
+    {
+      "StationName": "Stadshagen",
+      "SiteId": "9307",
+      "X": "18015938",
+      "Y": "59337462"
+    },
+    {
+      "StationName": "Stora mossen",
+      "SiteId": "9111",
+      "X": "17967423",
+      "Y": "59334783"
+    },
+    {
+      "StationName": "Stureby",
+      "SiteId": "9164",
+      "X": "18054178",
+      "Y": "59274421"
+    },
+    {
+      "StationName": "Sundbybergs centrum",
+      "SiteId": "9325",
+      "X": "17971495",
+      "Y": "59360870"
+    },
+    {
+      "StationName": "Svedmyra",
+      "SiteId": "9165",
+      "X": "18068983",
+      "Y": "59278187"
+    },
+    {
+      "StationName": "Sätra",
+      "SiteId": "9288",
+      "X": "17923448",
+      "Y": "59284480"
+    },
+    {
+      "StationName": "Tallkrogen",
+      "SiteId": "9184",
+      "X": "18087330",
+      "Y": "59268560"
+    },
+    {
+      "StationName": "T-Centralen",
+      "SiteId": "9001",
+      "X": "18060434",
+      "Y": "59331376"
+    },
+    {
+      "StationName": "Tekniska högskolan",
+      "SiteId": "9204",
+      "X": "18071707",
+      "Y": "59345543"
+    },
+    {
+      "StationName": "Telefonplan",
+      "SiteId": "9263",
+      "X": "17997510",
+      "Y": "59298233"
+    },
+    {
+      "StationName": "Tensta",
+      "SiteId": "9321",
+      "X": "17904723",
+      "Y": "59393959"
+    },
+    {
+      "StationName": "Thorildsplan",
+      "SiteId": "9114",
+      "X": "18017268",
+      "Y": "59331664"
+    },
+    {
+      "StationName": "Universitetet",
+      "SiteId": "9203",
+      "X": "18053800",
+      "Y": "59365365"
+    },
+    {
+      "StationName": "Vårberg",
+      "SiteId": "9286",
+      "X": "17887931",
+      "Y": "59273603"
+    },
+    {
+      "StationName": "Vårby gård",
+      "SiteId": "9285",
+      "X": "17886520",
+      "Y": "59263517"
+    },
+    {
+      "StationName": "Vällingby",
+      "SiteId": "9103",
+      "X": "17871535",
+      "Y": "59363486"
+    },
+    {
+      "StationName": "Västertorp",
+      "SiteId": "9261",
+      "X": "17966506",
+      "Y": "59291329"
+    },
+    {
+      "StationName": "Västra skogen",
+      "SiteId": "9306",
+      "X": "18003029",
+      "Y": "59347584"
+    },
+    {
+      "StationName": "Zinkensdamm",
+      "SiteId": "9296",
+      "X": "18050277",
+      "Y": "59317578"
+    },
+    {
+      "StationName": "Åkeshov",
+      "SiteId": "9108",
+      "X": "17923700",
+      "Y": "59342316"
+    },
+    {
+      "StationName": "Ängbyplan",
+      "SiteId": "9107",
+      "X": "17907034",
+      "Y": "59342568"
+    },
+    {
+      "StationName": "Örnsberg",
+      "SiteId": "9292",
+      "X": "17988215",
+      "Y": "59305182"
+    },
+    {
+      "StationName": "Östermalmstorg",
+      "SiteId": "9206",
+      "X": "18080049",
+      "Y": "59336365"
+    }
+  ]
